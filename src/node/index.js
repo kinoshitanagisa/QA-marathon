bea67fb08775e81f5caef8f42ee16711e7668e33
NagisaKinoshita <nagisa.kinoshita@rakus-partners.co.jp>
2024-01-09 17:45:54 +0900

    9KM突破

--- src/node/index.js
+++ src/node/index.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const port = 自分の社員番号;
+const port = 3518;
 
-const cors = require("cors");
-app.use(cors());
+const cors = require("cors");　
+//app.use(cors());
 
+app.use(cors({      
+  origin: '*',　　　//全てのオリジンからのアクセスを許可する　なぎさ追記　
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  //全てのメソッドOK　なぎさ追記
+}));　
+
+
+
+// PostgreSQLの接続プール（connection pool）を作成
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "localhost",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_3518", // PostgreSQLのユーザー名に置き換えてください
+  host: "db", //localhost（自分自身）→ postgresqlのホスト名dbに変更
+  database: "crm_3518", // PostgreSQLのデータベース名に置き換えてください
+  password: "pass_3518", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -37,7 +45,7 @@ app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",　//company_namを修正12/12（木下）
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3518' //APIサーバーのベースURLのポート番号3000を変更　12/11木下 //ホスト名localhost → dbに修正　12/12（木下）
 };
 
 export default config;
